d8608ed1a08f889d7f5d642d6a5d6c7642efb36a
minicoderwen <373441770@qq.com>
2023-11-08 05:09:52 +0000

    switch back to alacritty

--- macos/astronvim/lua/user/plugins/colorscheme.lua
+++ macos/astronvim/lua/user/plugins/colorscheme.lua
@@ -1,30 +1,38 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-        flavour = "mocha",
-        transparent_background = false,
-        term_colors = true,
-        integrations = {
-            alpha = true,
-            cmp = true,
-            gitsigns = true,
-            treesitter = true,
-            neotree = true,
-            mini = {
-                enabled = true,
-                indentscope_color = "",
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {
+            flavour = "mocha",
+            transparent_background = false,
+            term_colors = true,
+            integrations = {
+                alpha = true,
+                cmp = true,
+                gitsigns = true,
+                treesitter = true,
+                neotree = true,
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
+                },
+                mason = true,
+                notify = true,
+                treesitter_context = true,
+                telescope = {
+                    enabled = true,
+                    style = "nvchad",
+                },
+                which_key = true,
+                lsp_trouble = true,
+                headlines = true,
             },
-            mason = true,
-            notify = true,
-            treesitter_context = true,
-            telescope = {
-                enabled = true,
-                style = "nvchad",
-            },
-            which_key = true,
-            lsp_trouble = true,
-            headlines = true,
         },
     },
+
+    {
+        "rebelot/kanagawa.nvim",
+    },
+
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
 }
