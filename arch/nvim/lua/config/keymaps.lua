532d37b2b6b583980a2cf6e5a50f403f88e57e2f
minicoderwen <373441770@qq.com>
2023-11-09 21:04:27 +0000

    temp

--- arch/nvim/lua/config/keymaps.lua
+++ arch/nvim/lua/config/keymaps.lua
@@ -1,17 +1,14 @@
-return {
-    "folke/which-key.nvim",
-    config = function()
-        local wk = require "which-key"
-        wk.register {
-            -- Standard Operations
-            ["j"] = { "v:count == 0 ? 'gj' : 'j'", expr = true, desc = "Move cursor down" },
-            ["k"] = { "v:count == 0 ? 'gk' : 'k'", expr = true, desc = "Move cursor up" },
-            ["<leader>q"] = { "<cmd>confirm q<cr>", desc = "Quit" },
-            ["<leader>n"] = { "<cmd>enew<cr>", desc = "New File" },
-            ["<C-s>"] = { "<cmd>w!<cr>", desc = "Force write" },
-            ["<C-q>"] = { "<cmd>qa!<cr>", desc = "Force quit" },
-            ["|"] = { "<cmd>vsplit<cr>", desc = "Vertical Split" },
-            ["\\"] = { "<cmd>split<cr>", desc = "Horizontal Split" },
-        }
-    end,
-}
+local map = vim.api.nvim_set_keymap
+-- 复用 opt 参数
+local opt = {noremap = true, silent = true }
+
+maps.n["j"] = { "v:count == 0 ? 'gj' : 'j'", expr = true, desc = "Move cursor down" }
+maps.n["k"] = { "v:count == 0 ? 'gk' : 'k'", expr = true, desc = "Move cursor up" }
+maps.n["<leader>w"] = { "<cmd>w<cr>", desc = "Save" }
+maps.n["<leader>q"] = { "<cmd>confirm q<cr>", desc = "Quit" }
+maps.n["<leader>n"] = { "<cmd>enew<cr>", desc = "New File" }
+maps.n["<C-s>"] = { "<cmd>w!<cr>", desc = "Force write" }
+maps.n["<C-q>"] = { "<cmd>qa!<cr>", desc = "Force quit" }
+maps.n["|"] = { "<cmd>vsplit<cr>", desc = "Vertical Split" }
+maps.n["\\"] = { "<cmd>split<cr>", desc = "Horizontal Split" }
+
--- arch/nvim/lua/plugins/core.lua
+++ arch/nvim/lua/plugins/core.lua
@@ -7,5 +7,6 @@ return {
             vim.o.timeout = true
             vim.o.timeoutlen = 300
         end,
+        opts={}
     },
 }
