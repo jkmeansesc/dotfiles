ab5f1dd60e1010fd439ea177ad24c46c5c2474eb
minicoderwen <373441770@qq.com>
2023-11-10 03:53:32 +0000

    update nvim config

--- arch/nvim/init.lua
+++ arch/nvim/init.lua
@@ -2,7 +2,7 @@ for _, source in ipairs {
     "wen.keymaps",
     "wen.options",
     "wen.utils",
-    "wen.lazy",
+    "wen",
 } do
     local status_ok, fault = pcall(require, source)
     if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
