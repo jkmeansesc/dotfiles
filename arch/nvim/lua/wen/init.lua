f034cee23c4b2491dfeb02d3c0f46da023126f50
minicoderwen <373441770@qq.com>
2023-11-10 15:59:52 +0000

    add nvim alpha

--- arch/nvim/lua/wen/init.lua
+++ arch/nvim/lua/wen/init.lua
@@ -1,16 +1,15 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath })
+    -- bootstrap lazy.nvim
+    -- stylua: ignore
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+        lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup {
     spec = {
         -- { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-        { import = "wen.plugins" },
         { import = "wen.plugins.core" },
         { import = "wen.plugins.ui" },
     },
--- arch/nvim/lua/wen/plugins/ui/alpha-nvim.lua
+++ arch/nvim/lua/wen/plugins/ui/alpha-nvim.lua
@@ -1,99 +1,71 @@
 return {
     "goolord/alpha-nvim",
-    requires = { "kyazdani42/nvim-web-devicons" },
-    config = function()
-        local alpha = require "alpha"
+    event = "VimEnter",
+    enabled = true,
+    init = false,
+    opts = function()
         local dashboard = require "alpha.themes.dashboard"
+        local logo = [[
+         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
+         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
+         ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
+         ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
+         ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
+    ]]
 
-        math.randomseed(os.time())
-
-        local function pick_color()
-            local colors = { "String", "Identifier", "Keyword", "Number" }
-            return colors[math.random(#colors)]
-        end
-
-        local function footer()
-            local total_plugins = vim.tbl_keys(packer_plugins)
-            local datetime = os.date " %d-%m-%Y   %H:%M:%S"
-            local version = vim.version()
-            local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
-
-            return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
-        end
-
-        local logo = {
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "      ░░                                                  ░░      ",
-            "      ████                                              ████      ",
-            "      ██▓▓██                                          ██  ██      ",
-            "      ██▓▓▓▓██                                      ██    ██      ",
-            "  ██████▓▓▓▓▓▓██████████████████████████████████████      ██████  ",
-            "  ██░░░░░░▓▓▓▓▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░      ░░░░░░██  ",
-            "  ██▓▓▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒  ██  ",
-            "  ██▓▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒      ░░▒▒▒▒    ██  ",
-            "  ██░░▓▓▓▓▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒    ░░██  ",
-            "  ██░░▓▓▓▓▓▓▓▓▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒        ░░██  ",
-            "  ██░░▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒        ▒▒░░██  ",
-            "  ██▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒░░▒▒▒▒▒▒    ▒▒▒▒        ▒▒▒▒  ██  ",
-            "  ██▓▓▓▓▓▓▒▒▒▒▓▓▓▓▒▒▒▒▒▒▓▓▓▓▒▒▒▒░░▒▒▒▒    ▒▒▒▒▒▒    ▒▒▒▒      ██  ",
-            "  ██░░▓▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▓▓▒▒▒▒░░▒▒    ░░▒▒  ▒▒  ░░▒▒      ░░██  ",
-            "  ██▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▓▓    ▒▒  ▒▒▒▒    ▒▒▒▒▒▒      ▒▒  ██  ",
-            "  ██▓▓▓▓▒▒▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▒▒      ▒▒▒▒▒▒      ▒▒▒▒      ▒▒    ██  ",
-            "  ██░░▓▓▓▓▒▒▒▒▓▓▒▒▓▓▒▒▒▒▓▓        ▒▒▒▒    ▒▒▒▒  ▒▒  ▒▒▒▒    ░░██  ",
-            "  ██░░▓▓▓▓▓▓▓▓▒▒▒▒▓▓▒▒▒▒      ░░░░▒▒▒▒  ░░▒▒    ▒▒▒▒        ░░██  ",
-            "  ██░░▒▒▓▓▓▓▓▓▓▓▒▒▒▒▓▓▓▓    ▒▒▒▒░░  ▒▒▒▒▒▒    ▒▒▒▒        ▒▒░░██  ",
-            "  ██░░░░░░▓▓▓▓▓▓░░░░░░▓▓  ░░░░      ░░      ░░░░░░      ░░░░░░██  ",
-            "  ██░░▒▒▒▒▒▒▓▓▓▓░░▒▒▒▒▒▒▒▒▒▒      ░░▒▒    ░░    ▒▒    ░░▒▒▒▒░░██  ",
-            "  ██░░▓▓▓▓▒▒▒▒▓▓▒▒▓▓▓▓▓▓▒▒    ▒▒░░  ▒▒▒▒▒▒      ▒▒  ▒▒▒▒    ░░██  ",
-            "  ██░░▒▒▓▓▓▓▓▓▒▒▒▒▒▒▓▓▓▓    ▒▒      ▒▒▒▒      ▒▒▒▒▒▒      ▒▒░░██  ",
-            "  ██░░▒▒▓▓▓▓▓▓▓▓▒▒▓▓▒▒▓▓  ▒▒      ▒▒▒▒      ▒▒  ▒▒        ▒▒░░██  ",
-            "  ██░░▒▒▒▒▒▒▓▓▓▓▒▒▓▓▓▓▒▒▒▒    ▒▒░░  ▒▒    ▒▒    ▒▒    ▒▒▒▒▒▒░░██  ",
-            "  ██░░▒▒▓▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒▒  ▒▒      ▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒  ▒▒░░██  ",
-            "  ██░░▒▒▓▓▓▓▓▓▓▓▒▒▒▒▒▒▓▓  ▒▒      ▒▒▒▒      ▒▒▒▒▒▒        ▒▒░░██  ",
-            "  ██░░▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒░░    ▒▒░░▒▒▒▒    ░░▒▒  ▒▒      ▒▒▒▒░░██  ",
-            "  ██░░▒▒▒▒▒▒▒▒▓▓▒▒▓▓▓▓▓▓▒▒  ▒▒      ▒▒▒▒▒▒      ▒▒  ▒▒▒▒▒▒▒▒░░██  ",
-            "  ██░░▒▒▒▒▓▓▓▓▒▒▒▒▒▒▓▓▓▓  ▒▒      ▒▒▒▒        ▒▒▒▒▒▒    ▒▒▒▒░░██  ",
-            "  ██░░▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒░░  ▒▒▒▒░░▒▒▒▒    ░░▒▒▒▒      ░░▒▒▒▒░░██  ",
-            "  ██░░▒▒▒▒▒▒▒▒▓▓▓▓▒▒▓▓▓▓  ▒▒▒▒      ▒▒▒▒▒▒    ▒▒    ▒▒▒▒▒▒▒▒░░██  ",
-            "  ██░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒  ▒▒░░▒▒        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░██  ",
-            "  ██░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒    ▒▒    ░░▒▒▒▒      ▒▒▒▒▒▒▒▒░░██  ",
-            "  ██░░░░▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒      ░░▒▒▒▒      ▒▒    ▒▒▒▒▒▒▒▒░░░░██  ",
-            "  ██████░░░░▒▒▒▒▒▒▓▓▒▒▒▒    ▒▒▒▒░░        ▒▒▒▒  ▒▒▒▒▒▒░░░░██████  ",
-            "        ████░░░░▒▒▒▒▒▒    ░░          ▒▒░░▒▒▒▒▒▒▒▒░░░░████        ",
-            "            ████░░░░▒▒      ▒▒    ▒▒▒▒▓▓▓▓▓▓▒▒░░░░████            ",
-            "                ██      ▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▓▓▓▓▓▓██                ",
-            "              ██    ████░░░░▒▒▒▒░░▒▒▒▒░░░░████▓▓▓▓██              ",
-            "            ██    ██    ████░░░░░░░░░░████    ██▓▓▓▓██            ",
-            "          ████████          ████░░████          ████████          ",
-            "                                ██                                ",
-        }
-
-        dashboard.section.header.val = logo
-        dashboard.section.header.opts.hl = pick_color()
-
+        dashboard.section.header.val = vim.split(logo, "\n")
+        -- stylua: ignore
         dashboard.section.buttons.val = {
-            dashboard.button("<Leader>ff", "  File Explorer"),
-            dashboard.button("<Leader>fo", "  Find File"),
-            dashboard.button("<Leader>fw", "  Find Word"),
-            dashboard.button("<Leader>ps", "  Update plugins"),
-            dashboard.button("q", "  Quit", ":qa<cr>"),
+            dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+            dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+            dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+            dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
+            dashboard.button("c", " " .. " Config",
+                "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
+            dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+            dashboard.button("x", " " .. " Lazy Extras", "<cmd> LazyExtras <cr>"),
+            dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+            dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
         }
+        for _, button in ipairs(dashboard.section.buttons.val) do
+            button.opts.hl = "AlphaButtons"
+            button.opts.hl_shortcut = "AlphaShortcut"
+        end
+        dashboard.section.header.opts.hl = "AlphaHeader"
+        dashboard.section.buttons.opts.hl = "AlphaButtons"
+        dashboard.section.footer.opts.hl = "AlphaFooter"
+        dashboard.opts.layout[1].val = 8
+        return dashboard
+    end,
+    config = function(_, dashboard)
+        -- close Lazy and re-open when the dashboard is ready
+        if vim.o.filetype == "lazy" then
+            vim.cmd.close()
+            vim.api.nvim_create_autocmd("User", {
+                once = true,
+                pattern = "AlphaReady",
+                callback = function() require("lazy").show() end,
+            })
+        end
 
-        dashboard.section.footer.val = footer()
-        dashboard.section.footer.opts.hl = "Constant"
+        require("alpha").setup(dashboard.opts)
 
-        alpha.setup(dashboard.opts)
-
-        vim.cmd [[ autocmd FileType alpha setlocal nofoldenable ]]
+        vim.api.nvim_create_autocmd("User", {
+            once = true,
+            pattern = "LazyVimStarted",
+            callback = function()
+                local stats = require("lazy").stats()
+                local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                dashboard.section.footer.val = "⚡ Neovim loaded "
+                    .. stats.loaded
+                    .. "/"
+                    .. stats.count
+                    .. " plugins in "
+                    .. ms
+                    .. "ms"
+                pcall(vim.cmd.AlphaRedraw)
+            end,
+        })
     end,
 }
