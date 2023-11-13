c60f252a9bc2104a4edad87e2cc947f873444940
minicoderwen <373441770@qq.com>
2023-11-13 02:06:32 +0000

    trying

--- arch/nvim/lua/wen/plugins/config/nvim-tree.lua
+++ arch/nvim/lua/wen/plugins/config/nvim-tree.lua
@@ -74,6 +74,7 @@ M.config = function(_, opts)
     local api = require "nvim-tree.api"
     api.events.subscribe(api.events.Event.FileCreated, function(file) vim.cmd("edit " .. file.fname) end)
 
+    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
     -- configure nvim-tree
     require("nvim-tree").setup(opts)
 end
--- arch/nvim/lua/wen/plugins/ui.lua
+++ arch/nvim/lua/wen/plugins/ui.lua
@@ -38,43 +38,45 @@ return {
         event = "VimEnter",
         enabled = true,
         init = false,
-        opts = function()
-            local dashboard = require "alpha.themes.dashboard"
-            local logo = [[
-                  ████ ██████           █████      ██
-                 ███████████             █████ 
-                 █████████ ███████████████████ ███   ███████████
-                █████████  ███    █████████████ █████ ██████████████
-               █████████ ██████████ █████████ █████ █████ ████ █████
-             ███████████ ███    ███ █████████ █████ █████ ████ █████
-            ██████  █████████████████████ ████ █████ █████ ████ ██████
-        ]]
-
-            dashboard.section.header.val = vim.split(logo, "\n")
-            -- stylua: ignore
-            dashboard.section.buttons.val = {
-                dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
-                dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
-                dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
-                dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
-                dashboard.button("c", " " .. " Config",
-                    "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
-                dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-                dashboard.button("x", " " .. " Lazy Extras", "<cmd> LazyExtras <cr>"),
-                dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
-                dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
-            }
-            for _, button in ipairs(dashboard.section.buttons.val) do
-                button.opts.hl = "AlphaButtons"
-                button.opts.hl_shortcut = "AlphaShortcut"
-            end
-            dashboard.section.header.opts.hl = "AlphaHeader"
-            dashboard.section.buttons.opts.hl = "AlphaButtons"
-            dashboard.section.footer.opts.hl = "AlphaFooter"
-            dashboard.opts.layout[1].val = 8
-            return dashboard
-        end,
-        config = require "wen.plugins.config.alpha-nvim",
+        opts = require("wen.plugins.config.alpha-nvim").opts,
+        config = require("wen.plugins.config.alpha-nvim").config,
+        -- opts = function()
+        --     local dashboard = require "alpha.themes.dashboard"
+        --     local logo = [[
+        --           ████ ██████           █████      ██
+        --          ███████████             █████ 
+        --          █████████ ███████████████████ ███   ███████████
+        --         █████████  ███    █████████████ █████ ██████████████
+        --        █████████ ██████████ █████████ █████ █████ ████ █████
+        --      ███████████ ███    ███ █████████ █████ █████ ████ █████
+        --     ██████  █████████████████████ ████ █████ █████ ████ ██████
+        -- ]]
+        --
+        --     dashboard.section.header.val = vim.split(logo, "\n")
+        --     -- stylua: ignore
+        --     dashboard.section.buttons.val = {
+        --         dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+        --         dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+        --         dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+        --         dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
+        --         dashboard.button("c", " " .. " Config",
+        --             "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
+        --         dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+        --         dashboard.button("x", " " .. " Lazy Extras", "<cmd> LazyExtras <cr>"),
+        --         dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+        --         dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+        --     }
+        --     for _, button in ipairs(dashboard.section.buttons.val) do
+        --         button.opts.hl = "AlphaButtons"
+        --         button.opts.hl_shortcut = "AlphaShortcut"
+        --     end
+        --     dashboard.section.header.opts.hl = "AlphaHeader"
+        --     dashboard.section.buttons.opts.hl = "AlphaButtons"
+        --     dashboard.section.footer.opts.hl = "AlphaFooter"
+        --     dashboard.opts.layout[1].val = 8
+        --     return dashboard
+        -- end,
+        --
         -- config = function(_, dashboard)
         --     -- close Lazy and re-open when the dashboard is ready
         --     if vim.o.filetype == "lazy" then
