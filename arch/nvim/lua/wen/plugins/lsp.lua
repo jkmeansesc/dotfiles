56cedfcb7d8e9451e13ab790431640ad1c54edb1
minicoderwen <373441770@qq.com>
2023-11-14 06:22:17 +0000

    update

--- arch/nvim/lua/wen/plugins/lsp.lua
+++ arch/nvim/lua/wen/plugins/lsp.lua
@@ -1,2 +1,29 @@
+local path = "wen.plugins.config"
+
 return {
-    }
+    {
+        "williamboman/mason.nvim",
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim",
+        },
+        config = require(path .. ".mason"),
+    },
+
+    {
+        "nvimtools/none-ls.nvim",
+        lazy = true,
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = { "jay-babu/mason-null-ls.nvim" },
+        config = require(path .. ".none-ls"),
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            { "antosha417/nvim-lsp-file-operations", config = true },
+        },
+        config = require(path .. ".nvim-lspconfig"),
+    },
+}
