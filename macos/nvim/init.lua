61c928f3d16da0381ffccb44946daad3240ee73f
minicoderwen <373441770@qq.com>
2023-11-25 01:50:28 +0000

    update dotfiles config

--- macos/nvim/init.lua
+++
@@ -1,7 +0,0 @@
--- set default options
-require "wen.core"
-require "wen.core.lazy"
-vim.cmd [[colorscheme catppuccin]]
-
--- load mapping
-require("wen.core.utils").load_mappings()
--- macos/nvim/lua/wen/plugins/inc-rename.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "smjonas/inc-rename.nvim", -- LSP renaming with immediate visual feedback
-  init = function() require("wen.core.utils").load_mappings "increname" end,
-  config = true,
-}
