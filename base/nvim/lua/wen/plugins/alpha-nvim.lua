9eac69a8735c4b62bc3afdfc6747e72f1624fc7a
minicoderwen <373441770@qq.com>
2023-11-29 15:33:04 +0000

    update nvim

--- base/nvim/lua/wen/plugins/alpha-nvim.lua
+++ base/nvim/lua/wen/plugins/alpha-nvim.lua
@@ -17,12 +17,12 @@ return {
     dashboard.section.header.val = vim.split(logo, "\n")
     -- stylua: ignore
     dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
-        dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
-        dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
-        dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
-        dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
-        dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+      dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+      dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+      dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+      dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
+      dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+      dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
     }
     for _, button in ipairs(dashboard.section.buttons.val) do
       button.opts.hl = "AlphaButtons"
