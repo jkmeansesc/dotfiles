8a869bbb6cb7bf636a43edfba904cc5a56178c0e
minicoderwen <373441770@qq.com>
2024-01-28 16:54:44 +0000

    update hammerspoon

--- macos/hammerspoon/init.lua
+++ macos/hammerspoon/init.lua
@@ -1,6 +1,11 @@
--- install hammerspoon cli
-hs.ipc.cliInstall()
-
+-- load yabai keybinding
 require("yabai")
+
+-- load stackline
 local stackline = require("stackline")
 stackline:init()
+
+-- bind alt+ctrl+t to toggle stackline icons
+hs.hotkey.bind({ "alt", "ctrl" }, "t", function()
+	stackline.config:toggle("appearance.showIcons")
+end)
