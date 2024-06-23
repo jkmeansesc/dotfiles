bb4d13e899dff45eb7ba2c0dafc18479846068e4
jkmeansesc <373441770@qq.com>
2024-06-23 22:58:54 +0100

    add oil to nvim

---
+++ base/nvim/lua/archive/oil.lua
@@ -0,0 +1,15 @@
+return {
+    "stevearc/oil.nvim",
+    keys = {
+        { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+    },
+    config = function()
+        require("oil").setup {
+            delete_to_trash = true,
+            skip_confirm_for_simple_edits = true,
+            view_options = {
+                show_hidden = true,
+            },
+        }
+    end,
+}
