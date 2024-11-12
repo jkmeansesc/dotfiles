29279d40e2fa0973cd1a62b0639fdf82dd3b333c
jkmeansesc <373441770@qq.com>
2024-11-12 18:01:20 +0800

    update devcontainer

--- base/nvim/lua/lspsettings/yamlls.lua
+++ base/nvim/lua/lspsettings/yamlls.lua
@@ -1,9 +1,14 @@
 return {
     settings = {
         yaml = {
-            schemas = {
-                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+            schemaStore = {
+                -- You must disable built-in schemaStore support if you want to use
+                -- this plugin and its advanced options like `ignore`.
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = "",
             },
+            schemas = require("schemastore").yaml.schemas(),
         },
     },
 }
