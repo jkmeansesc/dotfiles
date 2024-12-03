39ef680636aa4dd335cbd4f05f8d44cf253431a5
jkmeansesc <373441770@qq.com>
2024-12-04 07:00:03 +0800

    update nvim

--- configs/nvim/lua/plugin/vim-oscyank.lua
+++ configs/nvim/lua/plugin/vim-oscyank.lua
@@ -2,7 +2,8 @@ return {
   "ojroques/vim-oscyank",
   event = { "BufReadPre", "BufNewFile" },
   keys = {
-    { "<Leader>y", "<Plug>OSCYankOperator", mode = { "n", "v" }, desc = "OSCYank" },
+    { "<Leader>y", "<Plug>OSCYankOperator", mode = "n", desc = "OSCYank" },
+    { "<Leader>y", "<Plug>OSCYankVisual", mode = "v", desc = "OSCYank Visual" },
     { "<Leader>Y", "<leader>c_", mode = "n", desc = "OSCYank line", remap = true },
   },
 }
