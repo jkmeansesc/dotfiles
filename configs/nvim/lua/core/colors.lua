cbc49527f538f8663cc6d9e1a64b43919f0db728
jkmeansesc <373441770@qq.com>
2024-12-22 12:14:11 +0800

    update nvim

---
+++ configs/nvim/lua/core/colors.lua
@@ -0,0 +1,34 @@
+local p = require "core.palette"
+local c = p.catppuccin
+local o = p.onedark
+local k = p.kanagawa
+local t = p.tokyonight
+
+return {
+  rosewrter = c.rosewater,
+  flamingo = c.flamingo,
+  pink = c.pink,
+  mauve = c.mauve,
+  red = c.red,
+  maroon = c.maroon,
+  peach = c.peach,
+  yellow = c.yellow,
+  green = c.green,
+  teal = c.teal,
+  sky = c.sky,
+  sapphire = c.sappphire,
+  blue = c.blue,
+  lavender = t.fg,
+  text = c.text,
+  subtext1 = c.subtext1,
+  subtext0 = c.subtext0,
+  overlay2 = c.overlay2,
+  overlay1 = c.overlay1,
+  overlay0 = c.overlay0,
+  surface2 = c.surface2,
+  surface1 = c.surface1,
+  surface0 = c.surface0,
+  base = c.base,
+  mantle = c.mantle,
+  crust = c.crust,
+}
