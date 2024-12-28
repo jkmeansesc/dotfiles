de7173795a4716d1bff403bacebb573673d64bed
jkmeansesc <373441770@qq.com>
2024-12-28 13:59:07 +0800

    update: nvim

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -11,28 +11,26 @@ require "core.keymaps"
 -- load autocmd
 require "core.autocmd"
 
-----------------------plugins spec start-----------------------
-
--- ╭──────────────────────────────────────────────────────────╮
--- │ ESSENTIALS                                               │
--- ╰──────────────────────────────────────────────────────────╯
+---------------------
+-- ESSENTIALS
+---------------------
 spec "plugin.nvim-treesitter"
-spec "plugin.telescope"
+spec "plugin.fzf-lua"
+spec "plugin.nvim-tree"
 spec "plugin.which-key"
-spec "plugin.gitsigns"
-spec "plugin.vim-fugitive"
+spec "plugin.blink"
 spec "plugin.conform"
 spec "plugin.nvim-lint"
-spec "plugin.blink"
 spec "plugin.nvim-lspconfig"
 spec "plugin.nvim-dap" -- TODO:
 
--- ╭──────────────────────────────────────────────────────────╮
--- │ EDITOR                                                   │
--- ╰──────────────────────────────────────────────────────────╯
-spec "plugin.nvim-tree"
+---------------------
+-- EDITOR
+---------------------
+spec "plugin.vim-fugitive"
+spec "plugin.gitsigns"
+spec "plugin.vim-tmux-navigator"
 spec "plugin.better-escape" -- jk means esc
-spec "plugin.smart-splits" -- necessary for tmux navigating across panes
 spec "plugin.inc-rename"
 spec "plugin.carbon-now" -- screenshot code
 spec "plugin.pangu" -- auto format to add a space between cjk and english letters
@@ -44,9 +42,9 @@ spec "plugin.vim-oscyank" -- yank text even from remote
 spec "plugin.ts-comments" -- enhance neovim's native comments, neovim natively supports comment now
 spec "plugin.snacks"
 
--- ╭──────────────────────────────────────────────────────────╮
--- │ UI                                                       │
--- ╰──────────────────────────────────────────────────────────╯
+---------------------
+-- UI
+---------------------
 spec "plugin.barbecue" -- VS Code like winbar
 spec "plugin.lualine"
 spec "plugin.nvim-colorizer"
@@ -54,8 +52,6 @@ spec "plugin.render-markdown"
 spec "plugin.todo-comments"
 spec "plugin.catppuccin"
 spec "plugin.smear-cursor"
-
-----------------------plugins spec end-------------------------
 
 -- bootstrap lazy
 require "core.lazy"
--- configs/nvim/lua/core/utils.lua
+++ configs/nvim/lua/core/utils.lua
@@ -74,15 +74,57 @@ end
 function M.on_attach(client)
   local map = M.map
   map("n", "gh", vim.diagnostic.open_float, { desc = "[G]oto [H]over Diagnostic" })
-  map({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, { desc = "Code [A]ction" })
-  map("n", "gd", require("telescope.builtin").lsp_definitions, { desc = "[G]oto [D]efinition" })
+  map({ "n", "v" }, "<Leader>la", "<CMD>FzfLua lsp_code_actions<CR>", { desc = "Code [A]ction" })
+  map(
+    "n",
+    "gd",
+    "<CMD>FzfLua lsp_definitions jump_to_single_result=true ignore_current_line=true<CR>",
+    { desc = "[G]oto [D]efinition" }
+  )
   map("n", "gD", vim.lsp.buf.declaration, { desc = "[G]oto [D]eclaration" })
-  map("n", "gi", require("telescope.builtin").lsp_implementations, { desc = "[G]oto [I]mplementation" })
-  map("n", "gr", require("telescope.builtin").lsp_references, { desc = "[G]oto [R]eferences" })
+  map(
+    "n",
+    "gi",
+    "<CMD>FzfLua lsp_implementations jump_to_single_result=true ignore_current_line=true<CR>",
+    { desc = "[G]oto [I]mplementation" }
+  )
+  map(
+    "n",
+    "gr",
+    "<CMD>FzfLua lsp_references jump_to_single_result=true ignore_current_line=true<CR>",
+    { desc = "[G]oto [R]eferences" }
+  )
   map("n", "<Leader>lh", vim.lsp.buf.signature_help, { desc = "Signature [H]elp" })
-  map("n", "<Leader>ld", require("telescope.builtin").lsp_type_definitions, { desc = "Type [D]efinition" })
-  map("n", "<leader>ls", require("telescope.builtin").lsp_document_symbols, { desc = "Document [S]ymbols" })
-  map("n", "<leader>lS", require("telescope.builtin").lsp_dynamic_workspace_symbols, { desc = "Workspace [S]ymbols" })
+  map(
+    "n",
+    "gy",
+    "<CMD>FzfLua lsp_typedefs jump_to_single_result=true ignore_current_line=true<CR>",
+    { desc = "T[y]pe Definition" }
+  )
+  map(
+    "n",
+    "<leader>ls",
+    "<CMD>FzfLua lsp_document_symbols jump_to_single_result=true ignore_current_line=true<CR>",
+    { desc = "Document [S]ymbols" }
+  )
+  map(
+    "n",
+    "<leader>lS",
+    "<CMD>FzfLua lsp_workspace_symbols jump_to_single_result=true ignore_current_line=true<CR>",
+    { desc = "Workspace [S]ymbols" }
+  )
+  map(
+    "n",
+    "<leader>ld",
+    "<CMD>FzfLua diagnostics_document jump_to_single_result=true ignore_current_line=true<CR>",
+    { desc = "Document [D]iagnostics " }
+  )
+  map(
+    "n",
+    "<leader>lD",
+    "<CMD>FzfLua diagnostics_workspace jump_to_single_result=true ignore_current_line=true<CR>",
+    { desc = "Workspace [D]iagnostics " }
+  )
   if client.supports_method "textDocument/codeLens" then
     map("n", "<Leader>ll", vim.lsp.codelens.refresh, { desc = "Code[L]ens refresh" })
     map("n", "<Leader>lL", vim.lsp.codelens.run, { desc = "Code[L]ens run" })
