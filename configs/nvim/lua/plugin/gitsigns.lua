afb471d97628e5aa89aeb01d6d48dab6d9f4fc34
jkmeansesc <373441770@qq.com>
2025-10-03 19:59:24 +0800

    update: nvim

--- configs/nvim/lua/plugin/gitsigns.lua
+++ configs/nvim/lua/plugin/gitsigns.lua
@@ -1,41 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
-  ft = { "gitcommit", "diff" },
-  keys = function()
-    local gs = require "gitsigns"
-    return {
-      {
-        "]h",
-        function()
-          if vim.wo.diff then return "]h" end
-          vim.schedule(function() gs.next_hunk() end)
-          return "<Ignore>"
-        end,
-        desc = "Next hunk",
-        expr = true,
-      },
-      {
-        "[h",
-        function()
-          if vim.wo.diff then return "[h" end
-          vim.schedule(function() gs.prev_hunk() end)
-          return "<Ignore>"
-        end,
-        desc = "Prev hunk",
-        expr = true,
-      },
-      { "<Leader>gr", function() gs.reset_hunk() end, desc = "[R]eset Hunk" },
-      { "<Leader>gR", function() gs.reset_buffer() end, desc = "[R]eset Buffer" },
-      { "<Leader>gp", function() gs.preview_hunk() end, desc = "[P]review Hunk" },
-      { "<Leader>gs", function() gs.stage_hunk() end, desc = "[S]tage Hunk" },
-      { "<Leader>gS", function() gs.stage_buffer() end, desc = "[S]tage Buffer" },
-      { "<Leader>gu", function() gs.undo_stage_hunk() end, desc = "[U]ndo Hunk" },
-      -- { "<Leader>gb", function() gs.blame_line() end, desc = "[B]lame Line" }, -- NOTE: use snacks
-      { "<Leader>gB", function() gs.blame_line { full = true } end, desc = "[B]lame Buffer" },
-      { "<Leader>gd", function() gs.diffthis() end, desc = "Git [D]iff" },
-      { "<Leader>gt", function() gs.toggle_current_line_blame() end, desc = "[T]oggle Line Blame" },
-    }
-  end,
+  event = { "BufReadPre", "BufNewFile" },
   opts = {
     signs = {
       add = { text = "▎" },
@@ -52,5 +17,33 @@ return {
       topdelete = { text = "" },
       changedelete = { text = "▎" },
     },
+    on_attach = function()
+      local gs = require "gitsigns"
+      local m = require("core.utils").map
+      m("n", "]h", function()
+        if vim.wo.diff then
+          vim.cmd.normal { "]c", bang = true }
+        else
+          gs.nav_hunk "next"
+        end
+      end, { desc = "Next Hunk" })
+      m("n", "[h", function()
+        if vim.wo.diff then
+          vim.cmd.normal { "[c", bang = true }
+        else
+          gs.nav_hunk "prev"
+        end
+      end, { desc = "Prev Hunk" })
+      m("n", "<Leader>gr", function() gs.reset_hunk() end, { desc = "[R]eset Hunk" })
+      m("n", "<Leader>gR", function() gs.reset_buffer() end, { desc = "[R]eset Buffer" })
+      m("n", "<Leader>gp", function() gs.preview_hunk() end, { desc = "[P]review Hunk" })
+      m({ "n", "v" }, "<Leader>gs", function() gs.stage_hunk() end, { desc = "[S]tage Hunk" })
+      m({ "n", "v" }, "<Leader>gS", function() gs.stage_buffer() end, { desc = "[S]tage Buffer" })
+      m("n", "<Leader>gu", function() gs.undo_stage_hunk() end, { desc = "[U]ndo Hunk" })
+      -- m("n", "<Leader>gb", function() gs.blame_line() end, { desc = "[B]lame Line" }) -- NOTE: use snacks
+      m("n", "<Leader>gB", function() gs.blame_line { full = true } end, { desc = "[B]lame Buffer" })
+      m("n", "<Leader>gd", function() gs.diffthis() end, { desc = "Git [D]iff" })
+      m("n", "<Leader>gt", function() gs.toggle_current_line_blame() end, { desc = "[T]oggle Line Blame" })
+    end,
   },
 }
